060f79dae73a4c600473edec374506b4008a1042
Ramanjaneyulu <ramanjaneyulu0205@gmail.com>
2024-02-19 17:43:01 +0530

    first commit

---
+++ src/Components/Component2.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+import PortalComponent from "./PortalComponent";
+import Modal from "./Modal";
+
+
+function Component2(){
+    let[showModal, updateShowModal] = useState(false);
+    function displayModal() {
+        updateShowModal(true);
+    }
+
+    function hideModal() {
+        updateShowModal(false);
+    }
+    return <div style={{width: 300, position: "relative"}}>
+        <h3>This is Component2</h3>
+        <p>This is a paragraph</p>
+        {/* <PortalComponent></PortalComponent> */}
+        <button onClick={displayModal}>Show Modal</button>
+        <Modal showModal={showModal} hideModal={hideModal}></Modal>
+    </div>
+}
+
+export default Component2;
